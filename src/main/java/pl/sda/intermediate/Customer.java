48d71b95859e835c10ce7aba605755519563642a
kf <k.falinski@sdacademy.pl>
2019-10-26 13:26:59 +0200

    customers begin

--- src/main/java/pl/sda/intermediate/Customer.java
+++ src/main/java/pl/sda/intermediate/Customer.java
@@ -1,9 +1,38 @@
 package pl.sda.intermediate;
 
+import java.math.BigDecimal;
+
 public class Customer {
-    int id;
+    Integer id;
     String firstName;
     String lastName;
-    int age;
-    double salary;
+    Integer age;
+    BigDecimal salary;
+
+    static Integer counter = 1;
+
+    {
+        this.id = counter++;
+    }
+    public Customer(String firstName, String lastName, Integer age, String salary) {
+
+        this(firstName, lastName, age, Double.parseDouble(salary));
+    }
+    public Customer(String firstName, String lastName, Integer age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = BigDecimal.valueOf(salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
 }
---
+++ src/test/java/pl/sda/intermediate/CustomerTest.java
@@ -0,0 +1,13 @@
+package pl.sda.intermediate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+    @Test
+    void name() {
+        Customer customer = new Customer();
+        System.out.println(customer);
+    }
+}
