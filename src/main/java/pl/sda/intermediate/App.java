58c4731b472b17920a215261e1c5e5f8a75f49a4
kf <k.falinski@sdacademy.pl>
2019-11-16 16:15:36 +0100

    hello world in spring boot

--- src/main/java/pl/sda/intermediate/App.java
+++ src/main/java/pl/sda/intermediate/App.java
@@ -2,9 +2,11 @@ package pl.sda.intermediate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-public class App {
+public class App { //todo
     public static void main(String[] args) {
         SpringApplication.run(App.class);
     }
--- src/main/java/pl/sda/intermediate/OnlyOneController.java
+++ src/main/java/pl/sda/intermediate/OnlyOneController.java
@@ -5,17 +5,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class OnlyOneController {
 
     @RequestMapping("/categories")
-    public String categoriesPage(@RequestParam String categoryName, Model model) {
+    public String categoriesPage(@RequestParam String input, Model model) {
         CategoryService categoryService = new CategoryService();
-        List<CategoryDTO> categories = categoryService.findCategories(categoryName);
+        List<CategoryDTO> categories = categoryService.findCategories(input);
         model.addAttribute("catsdata", categories);
         return "catspage"; //to jest nazwa strony html jaka nam sie wyswietli
+    }
+
+
+    private List<String> history = new ArrayList<>();
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(required = false) String personName, Model model) {
+        history.add(personName);
+        model.addAttribute("message", "Hello " + (personName == null ? "World" : personName));
+        model.addAttribute("history", history);
+        return "helloSite";
     }
 
 
